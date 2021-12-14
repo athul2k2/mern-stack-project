f0e00d51d1bd3765f1fefddebb16f036d9b506d8
athul krishna <athulkrishnapv6@gmail.com>
2021-12-14 12:23:50 +0530

    delete user and profile done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -171,6 +171,85 @@ router.get('/user/:user_id',async (req , res)=>{
 })
 
 
+//@route   DELETE  api/profile
+//@des     DELETE  profile, user & posts
+//@access  Private
+
+
+router.delete('/',auth, async (req , res)=>{
+    try {
+        // @todo -remove users posts
+        // Remove profile
+        await Profile.findOneAndRemove({user: req.user.id});
+        //remove user
+        await User.findOneAndRemove({_id: req.user.id});
+        res.json({msg: 'User deleted'});
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error');
+        
+    }
+})
+
+
+
+// @route  PUT api/profile/experience
+// @ desc  Add profile experience 
+// @access Private
+router.put('/experience ', [auth,[
+     check('title','Title is required')
+     .not()
+     .isEmpty(),
+     check('company','Company is required')
+     .not()
+     .isEmpty(),
+     check('from','From is required')
+     .not()
+     .isEmpty(),
+]], async (req, res) => {
+       const errors = validationResult(req);
+       if(!error.isEmpty()){
+           return res.status(400).json({errors: errors.array() });
+       }
+
+       const { 
+          title,
+          company,
+          location,
+          from,
+          to,
+          current,
+          description
+       } = req.body;
+                    
+       
+       const newExp ={
+           title,
+           company,
+           location,
+           from,
+           to,
+           current,
+           description
+
+       }
+
+       try {
+           const proflie = await Proflie.findOne({user: req.user.id});
+           
+           proflie.experience.unshift(newExp);
+
+           await profile.save();
+
+           res.json(profile);
+           
+       } catch (err) {
+           console.error(err.message);
+           res.status(500).send('Server Error');
+       }
+});
+
 
 
 
